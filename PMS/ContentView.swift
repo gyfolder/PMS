713587139a2a2f311e572e36ec4ff256ef96df34
Giyong Park <gyfolders@gmail.com>
2022-07-22 14:49:05 +0900

    Initial Commit

---
+++ PMS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PMS
+//
+//  Created by 박기용 on 2022/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PMS/PMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PMSApp.swift
+//  PMS
+//
+//  Created by 박기용 on 2022/07/22.
+//
+
+import SwiftUI
+
+@main
+struct PMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
